4a5dfda038aa0deb8251072062a61fc7ebd0366c
Christos Charmatzis <christos.charmatzis@navd.ai>
2019-12-13 23:16:21 +0200

    presentation ready

---
+++ src/config.template.py
@@ -0,0 +1,10 @@
+STORAGE_ACCOUNT_NAME = "athensaibootcampdemo"
+STORAGE_ACCOUNT_KEY = ""
+
+BATCH_ACCOUNT_NAME = "athensaibootcampdemo"
+BATCH_ACCOUNT_KEY = ""
+BATCH_ACCOUNT_URL = "https://athensaibootcampdemo.westeurope.batch.azure.com"
+
+ACR_LOGINSERVER = "athensaibootcampdemo.azurecr.io"
+ACR_USERNAME = "athensaibootcampdemo"
+ACR_PASSWORD = ""
